6910cd3e8abcca89f4b563147773f0776c74d454
kua-306 <thu3062003@gmail.com>
2026-03-18 11:19:41 +0700

    update db

--- api.py
+++ api.py
@@ -22,9 +22,9 @@ import os
 from dotenv import load_dotenv  
 load_dotenv()
 
-DATABASE_URL = os.getenv('db_url')
+DATABASE_URL = "sqlite+aiosqlite:///./sql_app.db"
 
-engine = create_engine(DATABASE_URL)
+engine = create_engine(DATABASE_URL, connect_args={"check_same_thread": False})
 Base.metadata.create_all(engine)
 
 app = FastAPI()
