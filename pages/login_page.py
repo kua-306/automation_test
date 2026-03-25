3e4cbc510ef476e50b7a35893d3a4e8f919d55e9
kua-306 <thu3062003@gmail.com>
2026-03-25 11:58:02 +0700

    add locator

--- pages/login_page.py
+++ pages/login_page.py
@@ -3,7 +3,7 @@ from pages.base_page import BasePage
 class LoginPage(BasePage):
     def __init__(self,page):
         super().__init__(page)
-        self.login_page = page.locator("#auth-section")
+        self.login_page = page.locator("#auth-section").click()
         self.username = self.login_page.get_by_role("textbox", name="Username")
         self.password = self.login_page.get_by_role("textbox", name="Password")
         self.login_btn = self.login_page.get_by_role("button", name="Đăng ký")
