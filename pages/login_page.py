bd5333126baba72904f91ff5c931ca3a01dca2b9
kua-306 <thu3062003@gmail.com>
2026-03-25 11:45:13 +0700

    fix

--- pages/login_page.py
+++ pages/login_page.py
@@ -3,11 +3,12 @@ from pages.base_page import BasePage
 class LoginPage(BasePage):
     def __init__(self,page):
         super().__init__(page)
-
         self.username = page.get_by_role("textbox", name="Username")
         self.password = page.get_by_role("textbox", name="Password")
         self.login_btn = page.get_by_role("button", name="Đăng nhập")
     def login(self,username,password):
+        self.username.click()
         self.username.fill(username)
+        self.password.click()
         self.password.fill(password)
         self.login_btn.click()
